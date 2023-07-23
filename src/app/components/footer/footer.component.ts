1bd662519c5eb33fe3eb1baa958fdbc2b39f5e2f
Mbalenhle Khokhoba <Mbalenhle.khokhoba@entelect.co.za>
2023-07-23 20:07:07 +0200

    Added email values

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+
+  // public invalid: boolean | null = false;
+
+  contactForm: FormGroup = new FormGroup({
+    name: new FormControl('', {
+      validators: [Validators.required, Validators.maxLength(50)],
+      updateOn: 'blur',
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur'
+    }),
+    message: new FormControl('', {
+      validators: [Validators.required, Validators.maxLength(300)],
+      updateOn: 'blur'
+    })
+  });
+
+  public sendEmail() {
+    let templateParams: any = {
+      from_mail: this.contactForm.controls['email'].value,
+      from_name: this.contactForm.controls['name'].value,
+      message: this.contactForm.controls['message'].value
+    }
+    emailjs.send('service_jyjgvlr', 'template_alsvk0k', templateParams, 'WU-amvWNJFrgfKIka')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
+
+}
