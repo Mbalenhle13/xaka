fee8f68c617db180f8da199f0386a6bc7198fa5a
Mbalenhle Khokhoba <Mbalenhle.khokhoba@entelect.co.za>
2023-07-30 01:03:03 +0200

    footer component update

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,8 +4,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  templateUrl: './footer.component.html'
 })
 export class FooterComponent {
 
