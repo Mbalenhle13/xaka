5749d9085766c6f09c73a567613a30a8ed9114ac
Mbalenhle Khokhoba <Mbalenhle.khokhoba@entelect.co.za>
2023-09-09 01:25:54 +0200

    updated validation, fixed defects & added toast alert

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,46 +1,43 @@
-import {Component, HostListener} from '@angular/core';
-import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import {Component, HostListener, OnInit} from '@angular/core';
+import emailjs from 'emailjs-com';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
-  public isCtrl = false;
-
-  contactForm: FormGroup = new FormGroup({
-    name: new FormControl('', {
-      validators: [Validators.required, Validators.maxLength(50)],
-      updateOn: 'blur',
-    }),
-    phone: new FormControl('', {
-      validators: [Validators.required,Validators.minLength(10), Validators.maxLength(10)],
-      updateOn: 'blur'
-    }),
-    message: new FormControl('', {
-      validators: [Validators.required, Validators.maxLength(300)],
-      updateOn: 'blur'
-    }),
-    email: new FormControl('', {
-      validators: [Validators.required, Validators.email],
-      updateOn: 'blur'
-    }),
-    subject: new FormControl('', {
-      validators: [Validators.required],
-      updateOn: 'blur'
-    })
-  });
+  contactForm!: FormGroup;
+  showSuccessToast: boolean = false;
+  showFailureToast: boolean = false;
+  ngOnInit() {
+     this.contactForm = new FormGroup({
+      name: new FormControl('', {
+        validators: [Validators.required, Validators.maxLength(50)],
+        updateOn: 'blur',
+      }),
+      phone: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(10), Validators.maxLength(10),Validators.pattern(/^[0-9]*$/)],
+        updateOn: 'blur'
+      }),
+      message: new FormControl('', {
+        validators: [Validators.required, Validators.maxLength(300)],
+        updateOn: 'blur'
+      }),
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')],
+        updateOn: 'blur'
+      }),
+      subject: new FormControl('', {
+        validators: [Validators.required],
+        updateOn: 'blur'
+      })
+    });
+  }
 
   @HostListener('paste', ['$event']) blockPaste(event: ClipboardEvent) {
-    if(this.contactForm.controls['phone'].value && !this.#isNumber(event.clipboardData!.getData('text')))
-    {
-      return false
-    }
-    else {
-      return true
-    }
+    return !(this.contactForm.controls['phone'].value && !this.#isNumber(event.clipboardData!.getData('text')));
   }
 
   sendEmail() {
@@ -51,10 +48,22 @@ export class FooterComponent {
       phone: this.contactForm.controls['phone'].value,
       subject: this.contactForm.controls['subject'].value
     }
-    emailjs.send('service_jyjgvlr', 'template_alsvk0k', templateParams, 'WU-amvWNJFrgfKIka')
-      .then((result: EmailJSResponseStatus) => {}, (error) => {
-        console.error(error.text);
+    if(this.contactForm.controls['email'].valid && this.contactForm.controls['name'].valid &&
+      this.contactForm.controls['message'].valid && this.contactForm.controls['phone'].valid &&
+      this.contactForm.controls['subject'].valid) {
+      emailjs.send('service_jyjgvlr', 'template_alsvk0k', templateParams, 'WU-amvWNJFrgfKIka')
+        .then(() => {
+        this.showFailureToast = false;
+        this.showSuccessToast = true;
+      }, () => {
+        this.showFailureToast = true;
+        this.showSuccessToast = false;
       });
+    } else {
+      this.showFailureToast = true;
+      this.showSuccessToast = false;
+    }
+    return;
   }
 
 
@@ -63,19 +72,20 @@ export class FooterComponent {
     const charCode = (event.which) ? event.which : event.keyCode;
 
     if (!regex){
-      console.log('Regex result: ', regex);
       return false;
     }
 
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-      return false;
-    }
-    return true;
+    return !(charCode > 31 && (charCode < 48 || charCode > 57));
+
   }
 
   #isNumber(value: string | number): boolean {
     return value != null && value !== '' && !isNaN(Number(value.toString()));
   }
 
+  hideAlerts() {
+    this.showFailureToast = false;
+    this.showSuccessToast = false;
+  }
 
 }
